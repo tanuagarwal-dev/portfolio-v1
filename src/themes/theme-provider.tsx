c6dc54fd24c3d1010796625c38df42e25aa9c532
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-08-31 18:55:24 +0530

    feat: enhance global styles and add theme provider for improved theming support

---
+++ src/themes/theme-provider.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Theme } from '@tsparticles/engine';
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from 'next-themes';
+
+export default function ThemesProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
