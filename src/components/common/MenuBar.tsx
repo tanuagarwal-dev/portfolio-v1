8cce694167025a60e0d8d9d796dcc2a0d0855ea0
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-08-31 19:16:31 +0530

    feat: add MenuBar component and integrate it into Header for improved navigation

---
+++ src/components/common/MenuBar.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Menu } from 'lucide-react';
+import Link from 'next/link';
+export const MenuBar = () => {
+  return (
+    <div>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button
+            variant="outline"
+            className="bg-transparent border-0 font-bold"
+          >
+            <Menu />
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-sm h-screen flex flex-col justify-start gap-4">
+            <DrawerHeader>
+              <DrawerTitle>Tanu Agarwal</DrawerTitle>
+              <DrawerDescription>Web Dev</DrawerDescription>
+            </DrawerHeader>
+
+            <div className="border p-4 capitalize m-2">
+              <ul>
+                <li>
+                  <Link
+                    href="#home"
+                    className="hover:text-green-600 dark:hover:text-green-400 transition"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#about"
+                    className="hover:text-green-600 dark:hover:text-green-400 transition"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#projects"
+                    className="hover:text-green-600 dark:hover:text-green-400 transition"
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#contact"
+                    className="hover:text-green-600 dark:hover:text-green-400 transition"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <DrawerClose asChild>
+              <Button variant="outline" className="text-red-600  font-bold m-4">
+                Cancel
+              </Button>
+            </DrawerClose>
+            {/* <DrawerFooter>
+            
+          </DrawerFooter> */}
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
+};
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,16 +1,55 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { MenuBar } from './MenuBar';
+import Switch from '../uiverse/Switch';
 
-export default function Header(){
-    return(
-        <div className="fixed top-4 left-0 mx-2 w-full flex justify-between px-8 z-20">
-            <div className="text-lg font-semibold">TA.</div>
-            <div className="flex gap-8 px-8 text-lg">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/contact">Contact</Link>
-                <span className="bg-amber-300">FR <span className="bg-amber-700">EN</span></span>
-            </div>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="fixed top-0 left-0 w-full flex justify-between items-center px-8 py-2 md:py-4 lg:py-4 z-50  bg-white/30 dark:bg-black/30  backdrop-blur-md border-b border-gray-300/30 dark:border-white/10 text-gray-900 dark:text-white transition-colors duration-300">
+      {/* Logo */}
+      <span className="text-lg font-semibold">TA.</span>
+
+      {/* Links */}
+      <span className="flex gap-6 items-center px-8 text-lg justify-between">
+        <span className="hidden md:flex lg:flex gap-8 px-8">
+          <Link
+            href="#home"
+            className="hover:text-green-600 dark:hover:text-green-400 transition"
+          >
+            Home
+          </Link>
+          <Link
+            href="#about"
+            className="hover:text-green-600 dark:hover:text-green-400 transition"
+          >
+            About
+          </Link>
+          <Link
+            href="#projects"
+            className="hover:text-green-600 dark:hover:text-green-400 transition"
+          >
+            Projects
+          </Link>
+          <Link
+            href="#contact"
+            className="hover:text-green-600 dark:hover:text-green-400 transition"
+          >
+            Contact
+          </Link>
+        </span>
+
+        {/* Mobile Menu */}
+        <span className="lg:hidden md:hidden my-2">
+          <MenuBar />
+        </span>
+        <span className="flex items-center gap-4">
+          <Switch />
+        </span>
+      </span>
+
+      <span className="text-sm bg-gray-200/60 dark:bg-white/10 rounded-md px-2 py-1">
+        <span className="text-amber-500">&lt;Code/&gt;</span>|
+        <span className="text-amber-700">&lt;Ship/&gt;</span>
+      </span>
+    </div>
+  );
 }
