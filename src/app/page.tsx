10f509d450edf14dae98949c39a60b0e86545c9a
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-08-31 19:16:43 +0530

    feat: update layout and page components for improved structure and theme integration; add missing font imports and enhance responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
-"use client"
-import Archive from "@/components/common/archive";
-import Contact from "@/components/common/contacts";
-import Header from "@/components/common/header";
-import Hero from "@/components/common/hero";
-import Intro from "@/components/common/intro";
+'use client';
 
+import Header from '@/components/common/header';
+import Landing from '@/components/v2/landing';
+import About from '@/components/v2/about';
+import Projects from '@/components/v2/projects';
+import Touch from '@/components/v2/touch';
+import Education from '@/components/v2/education';
+import Skills from '@/components/v2/skills';
+import TerminalComponent from '@/components/common/TerminalComponent';
 
 export default function Home() {
   return (
-    <div className="bg-radial from-[#d7ccbb] via-[#deb7a4] to-[#e9d5c7] text-black flex flex-col items-center justify-center lg:mx-40 md:mx-20 mx-4 my-20 border border-white/10 rounded-lg h-full">
-    <Header/>
-  <Hero/>
-  <Intro/>
-  <Archive/>
-  <Contact/>
-</div>
+    <div className="w-full min-h-screen p-4 dark:bg-black" id="home">
+      <div className="lg:mx-40 md:mx-20 mx-4 my-20 dark:border-white/10 border border-slate-200 rounded-lg h-full  flex flex-col gap-4 text-white">
+        <Header />
+        <Landing />
+        <About />
+        <Education />
+        <Projects />
+        <Skills />
+        <Touch />
+      </div>
+    </div>
   );
 }
